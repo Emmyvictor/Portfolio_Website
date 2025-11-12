fbe48ffbafedab580bac4026735c11c405579a06
Emmyvictor <emmydope539@gmail.com>
2025-11-12 13:52:15 +0100

    add new changes

--- app.py
+++ app.py
@@ -282,6 +282,6 @@ def internal_error(e):
     )
 
 
-# ---------- MAIN ----------
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's port if available
+    app.run(debug=False, host="0.0.0.0", port=port)
